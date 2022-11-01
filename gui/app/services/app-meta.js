e2b991c3b438ebb26ea2dfc22420bbf2d587981d
sukhdeep-lab <sukhdeep@malonetuning.com>
2022-11-01 14:10:13 -0700

    chore: remove theme hack

--- gui/app/services/app-meta.js
+++ gui/app/services/app-meta.js
@@ -138,8 +138,7 @@ export default Service.extend({
 		return this.get('ajax').request(`public/meta/themes`, {
 			method: 'GET'
 		}).then((response) => {
-			// TODO: Register/build with TZ theme in domain/meta/endpoint.go Themes request
-			return [ ...response, { name: 'TuneZilla', primary: '#2744f6' } ];
+			return response;
 		});
 	}
 });
